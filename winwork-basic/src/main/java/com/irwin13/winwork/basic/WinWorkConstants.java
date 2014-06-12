18a2469e9552a96f12bea872a89c4baddc98ed34
irwin <irwin13@gmail.com>
2014-06-12 10:59:57 +0700

    simplify missing node name message

--- winwork-basic/src/main/java/com/irwin13/winwork/basic/WinWorkConstants.java
+++ winwork-basic/src/main/java/com/irwin13/winwork/basic/WinWorkConstants.java
@@ -54,6 +54,6 @@ public final class WinWorkConstants {
     public static final String DEFAULT_PASSWORD = "123";
 
     public static final String WINWORK_NODE_NAME = "winWork.nodeName";
-    public static final String NODE_NAME_MISSING = "missing -D" + WINWORK_NODE_NAME + " argument in Java startup parameter";
+    public static final String NODE_NAME_MISSING = "missing '-D" + WINWORK_NODE_NAME + "' argument";
 
 }
